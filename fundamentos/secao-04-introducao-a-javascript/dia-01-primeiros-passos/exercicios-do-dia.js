bb175b528224748d92176a4075284be7c0ea61f6
Maria Gomes <mari-pgomes@outlook.com>
2022-11-23 15:31:47 -0300

    Resolução do exercício 1 do dia.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
@@ -0,0 +1,12 @@
+//Exercício 1
+
+const primeiroNumero = 7;
+const segundoNumero = 9;
+
+console.log(primeiroNumero + segundoNumero);
+console.log(primeiroNumero - segundoNumero);
+console.log(primeiroNumero * segundoNumero);
+console.log(primeiroNumero / segundoNumero);
+console.log(primeiroNumero % segundoNumero);
+
+//Exercício 2
