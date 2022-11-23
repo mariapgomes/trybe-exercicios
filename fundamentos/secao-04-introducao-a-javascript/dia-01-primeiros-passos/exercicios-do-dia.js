a3849404f527bb81b9f85e3bd741b2b64c42b485
Maria Gomes <mari-pgomes@outlook.com>
2022-11-23 15:51:54 -0300

    Resolução do exercício 3 do dia.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
@@ -2,6 +2,7 @@
 
 const primeiroNumero = 7;
 const segundoNumero = 9;
+const terceiroNumero = 15;
 
 // console.log(primeiroNumero + segundoNumero);
 // console.log(primeiroNumero - segundoNumero);
@@ -13,11 +14,28 @@ const segundoNumero = 9;
 
 let resultado = "";
 
-if (primeiroNumero > segundoNumero) {
+// if (primeiroNumero > segundoNumero) {
+//     resultado = "O numero " + primeiroNumero + " é o maior";
+// }
+// else if (segundoNumero > primeiroNumero) {
+//     resultado = "O numero " + segundoNumero + " é o maior";
+// }
+// else {
+//     resultado = "Os números são iguais";
+// }
+
+// console.log(resultado);
+
+//Exercício 3
+
+if (primeiroNumero > segundoNumero && primeiroNumero >terceiroNumero) {
     resultado = "O numero " + primeiroNumero + " é o maior";
 }
-else if (segundoNumero > primeiroNumero) {
+else if (segundoNumero > primeiroNumero && segundoNumero >terceiroNumero) {
     resultado = "O numero " + segundoNumero + " é o maior";
+}
+else if (terceiroNumero > primeiroNumero && terceiroNumero > segundoNumero) {
+    resultado = "O numero " + terceiroNumero + " é o maior";
 }
 else {
     resultado = "Os números são iguais";
@@ -25,5 +43,4 @@ else {
 
 console.log(resultado);
 
-//Exercício 3
-
+//Exercício 4
