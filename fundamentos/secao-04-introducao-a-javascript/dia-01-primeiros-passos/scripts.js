a6aefb32daecaa3006795fbdccec830d8b62cd07
Maria Gomes <mari-pgomes@outlook.com>
2022-11-23 10:27:01 -0300

    Resolução do exercício de fixação 4.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -24,14 +24,37 @@
 
 // Exercício 3
 
-const nota = 50;
+// const nota = 50;
 
-if (nota >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!");
-} 
-else if (nota < 80 && nota >= 60) {
-    console.log("Você está na nossa lista de espera");
-} 
-else {
-    console.log("Você foi reprovada(o)");
-}
+// if (nota >= 80) {
+//     console.log("Parabéns, você foi aprovada(o)!");
+// } 
+// else if (nota < 80 && nota >= 60) {
+//     console.log("Você está na nossa lista de espera");
+// } 
+// else {
+//     console.log("Você foi reprovada(o)");
+// }
+
+// Exercício 4
+
+// const currentHour = 22;
+// let message = "";
+
+// if (currentHour >= 22) {
+//     message = "Não deveríamos comer nada, é hora de dormir";
+// }
+// else if (currentHour >= 18 && currentHour < 22) {
+//     message = "Rango da noite, vamos jantar :D"
+// }
+// else if (currentHour >= 14 && currentHour < 18) {
+//     message = "Vamos fazer um bolo pro café da tarde?"
+// }
+// else if (currentHour >= 11 && currentHour < 14) {
+//     message = "Hora do almoço!!!"
+// }
+// else if (currentHour >= 4 && currentHour < 11) {
+//     message = "Hmmm, cheiro de café recém-passado"
+// }
+
+// console.log(message)
