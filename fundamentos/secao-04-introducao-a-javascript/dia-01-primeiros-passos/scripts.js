7ed081bfda60c0921ecd9f6a7a02494ebd3129a6
Maria Gomes <mari-pgomes@outlook.com>
2022-11-23 09:28:10 -0300

    Resolução do exercício de fixação 2.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,5 +1,23 @@
-const myName = "Maria";
-const birthCity = "São Gonçalo";
-let birthYear = 1997;
+// const myName = "Maria";
+// const birthCity = "São Gonçalo";
+// let birthYear = 1997;
 
-console.log(myName + " " + birthCity + " " + birthYear);
+// console.log(myName + " " + birthCity + " " + birthYear);
+
+// birthYear = 2023;
+
+// console.log(myName + " " + birthCity + " " + birthYear);
+
+// Não é possivel alterar valores de constantes.
+
+//Exercício 2
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log(area);
+
+const perimeter = 5 + 2 * 8;
+
+console.log(perimeter);
