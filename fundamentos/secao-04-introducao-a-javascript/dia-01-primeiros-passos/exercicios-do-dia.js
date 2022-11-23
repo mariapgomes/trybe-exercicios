2ffdff271d876e955f61f7eac704a04989b84209
Maria Gomes <mari-pgomes@outlook.com>
2022-11-23 15:44:54 -0300

    Resolução do exercício 2 do dia.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
@@ -3,10 +3,27 @@
 const primeiroNumero = 7;
 const segundoNumero = 9;
 
-console.log(primeiroNumero + segundoNumero);
-console.log(primeiroNumero - segundoNumero);
-console.log(primeiroNumero * segundoNumero);
-console.log(primeiroNumero / segundoNumero);
-console.log(primeiroNumero % segundoNumero);
+// console.log(primeiroNumero + segundoNumero);
+// console.log(primeiroNumero - segundoNumero);
+// console.log(primeiroNumero * segundoNumero);
+// console.log(primeiroNumero / segundoNumero);
+// console.log(primeiroNumero % segundoNumero);
 
 //Exercício 2
+
+let resultado = "";
+
+if (primeiroNumero > segundoNumero) {
+    resultado = "O numero " + primeiroNumero + " é o maior";
+}
+else if (segundoNumero > primeiroNumero) {
+    resultado = "O numero " + segundoNumero + " é o maior";
+}
+else {
+    resultado = "Os números são iguais";
+}
+
+console.log(resultado);
+
+//Exercício 3
+
