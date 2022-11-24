94d25bedcf14b4ca343a2e9bc064da14f99e8e35
Maria Gomes <mari-pgomes@outlook.com>
2022-11-24 09:14:06 -0300

    Resolução do exercício 10 do dia.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia.js
@@ -9,6 +9,8 @@
 // console.log(primeiroNumero / segundoNumero);
 // console.log(primeiroNumero % segundoNumero);
 
+//----------------------------------------
+
 //Exercício 2
 
 // const primeiroNumero = 5;
@@ -27,6 +29,8 @@
 // }
 
 // console.log(resultado);
+
+//----------------------------------------
 
 //Exercício 3
 
@@ -50,6 +54,8 @@
 
 // console.log(resultado);
 
+//----------------------------------------
+
 //Exercício 4
 
 // const numero = 0;
@@ -66,6 +72,8 @@
 // }
 
 // console.log(resultado);
+
+//----------------------------------------
 
 //Exercício 5
 
@@ -89,6 +97,8 @@
 // } else {
 //     console.log("Erro! Ângulo inválido.")
 // }
+
+//----------------------------------------
 
 //Exercício 6
 
@@ -126,6 +136,8 @@
 //         console.log('Erro, peça inválida!')
 // }
 
+//----------------------------------------
+
 //Exercício 7
 
 // Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
@@ -140,6 +152,8 @@
 //     isEven = true
 // }
 // console.log(isEven);
+
+//----------------------------------------
 
 //Exercício 8
 
@@ -156,18 +170,83 @@
 // }
 // console.log(isOdd);
 
+//----------------------------------------
+
 //Exercício 9
 
 // (custo*(20/100)) Calculo de regra de três que representa a porcentagem de 20% do custo, assim não da erro na hora de rodar o código.
 
-let custo = 15;
-let valorVenda = 25;
+// let custo = 15;
+// let valorVenda = 25;
 
 
-if (custo >= 0 && valorVenda >= 0) {
-    const custoTotal = custo + (custo*(20/100));
-    const resultado = (valorVenda - custoTotal) * 1000;
-    console.log(resultado);
+// if (custo >= 0 && valorVenda >= 0) {
+//     const custoTotal = custo + (custo*(20/100));
+//     const resultado = (valorVenda - custoTotal) * 1000;
+//     console.log(resultado);
+// } else {
+//     console.log("Erro, valores inválidos!");
+// }
+
+//-----------------------------------------------
+
+//Exercício 10
+
+// Utilize if/else para escrever um código que, dado um salário bruto, calcule o líquido a ser recebido.
+// Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.
+
+// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+
+// INSS (Instituto Nacional do Seguro Social)
+
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+
+// IR (Imposto de Renda)
+
+// Até R$ 1.903,98: isento de imposto de renda
+
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
+let aliquotaINSS;
+let aliquotaIR;
+let salarioBruto = 2000.00
+
+if (salarioBruto <= 1556.94) {
+    aliquotaINSS = salarioBruto * (8/100);
+} else if (salarioBruto <= 2594.92) {
+    aliquotaINSS = salarioBruto * (9/100);
+} else if (salarioBruto <= 5189.82) {
+    aliquotaINSS = salarioBruto * (11/100);
 } else {
-    console.log("Erro, valores inválidos!");
-}
+    aliquotaINSS = 570.88
+};
+
+let salarioBase = salarioBruto - aliquotaINSS;
+
+if (salarioBase <= 1903.98) {
+    aliquotaIR = 0;
+} else if (salarioBase <= 2826.65) {
+    aliquotaIR = (salarioBase * (7.5/100)) - 142.80;
+} else if (salarioBase <= 3751.05) {
+    aliquotaIR = (salarioBase * (15/100)) - 354.80;
+} else if (salarioBase <=  4664.68) {
+    aliquotaIR = (salarioBase * (22.5/100)) - 636,13;
+} else {
+    aliquotaIR = (salarioBase * (27.5/100)) - 869.36;
+};
+
+let salarioLiquido = salarioBase - aliquotaIR;
+
+console.log("Salário líquido: ", salarioLiquido);
