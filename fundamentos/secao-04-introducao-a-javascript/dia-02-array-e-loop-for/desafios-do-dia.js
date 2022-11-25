4d83b3ca04dd3c0274b61345ac0c0785facafa1d
Maria Gomes <mari-pgomes@outlook.com>
2022-11-25 11:08:20 -0300

    Lição 3 dos desafios do dia resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/desafios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/desafios-do-dia.js
@@ -11,12 +11,27 @@
 // Lição 2
 // Dessa forma é possilvel somar todos os valores de um array
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index +=1) {
+//     soma += numbers[index];
+    
+// }
+
+// console.log(soma);
+
+//-------------------------------------------
+
+// Lição 3 - Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for (let index = 0; index < numbers.length; index +=1) {
-    soma += numbers[index];
+    soma += numbers[index] / numbers.length;
     
 }
 
-console.log(soma);
+console.log(soma.toFixed(1));
