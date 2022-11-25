5388a8379f9691920166ce7c2b9137aabc05e64b
Maria Gomes <mari-pgomes@outlook.com>
2022-11-25 08:30:00 -0300

    Exercicios de fixação resolvidos.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/exercicios-de-fixacao.js
@@ -0,0 +1,24 @@
+// Lição 1
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//--------------------------------------
+
+// Lição 2
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+//--------------------------------------
+
+// Lição 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
