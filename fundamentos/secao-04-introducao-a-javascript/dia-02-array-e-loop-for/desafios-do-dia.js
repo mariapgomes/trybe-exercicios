7595bbcd142f28c4430ee5a81607966440f03d7c
Maria Gomes <mari-pgomes@outlook.com>
2022-11-25 11:15:35 -0300

    Lição 4 dos desafios do dia resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/desafios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/desafios-do-dia.js
@@ -25,13 +25,29 @@
 
 // Lição 3 - Calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+// o .toFixed() determina a quantidade de casas decimais após a vírgula que será exibida.
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index +=1) {
+//     soma += numbers[index] / numbers.length;
+// }
+
+// console.log(soma.toFixed(1));
+
+//-------------------------------------------
+
+// Lição 4 -Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for (let index = 0; index < numbers.length; index +=1) {
-    soma += numbers[index] / numbers.length;
-    
+    soma += numbers[index] / numbers.length;  
 }
-
-console.log(soma.toFixed(1));
+if (soma.toFixed(1) > 20){
+    console.log("valor maior que 20.");
+} else {
+    console.log("valor menor ou igual a 20.");
+}
