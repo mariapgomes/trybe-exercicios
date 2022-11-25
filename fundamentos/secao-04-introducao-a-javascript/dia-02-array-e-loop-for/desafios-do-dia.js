143fbf1d65389d7111690471fe87d7be893c8fe1
Maria Gomes <mari-pgomes@outlook.com>
2022-11-25 16:20:27 -0300

    Lição 5 dos desafios do dia resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/desafios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/desafios-do-dia.js
@@ -40,14 +40,29 @@
 
 // Lição 4 -Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index +=1) {
+//     soma += numbers[index] / numbers.length;  
+// }
+// if (soma.toFixed(1) > 20){
+//     console.log("valor maior que 20.");
+// } else {
+//     console.log("valor menor ou igual a 20.");
+// }
+
+//-------------------------------------------
+
+// Lição 5 - Descubra qual o maior valor contido no array e imprima-o.
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maiorNumero = 0;
 
 for (let index = 0; index < numbers.length; index +=1) {
-    soma += numbers[index] / numbers.length;  
+    if (numbers[index] > maiorNumero){
+        maiorNumero = numbers[index];
+    }
 }
-if (soma.toFixed(1) > 20){
-    console.log("valor maior que 20.");
-} else {
-    console.log("valor menor ou igual a 20.");
-}
+
+  console.log(maiorNumero);  
