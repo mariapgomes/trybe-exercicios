b520546f4b7a546ccb1b8734b52acd4644b09de0
Maria Gomes <mari-pgomes@outlook.com>
2022-11-25 10:03:26 -0300

    Lição 4 de fixação resolvido.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/exercicios-de-fixacao.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/exercicios-de-fixacao.js
@@ -18,7 +18,27 @@
 
 // Lição 3
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato')
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
 
-console.log(menu);
+// console.log(menu);
+
+//--------------------------------------
+
+// Lição 3
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index += 1) {
+//     console.log(groceryList[index])
+// }
+
+//--------------------------------------
+
+// Lição 4
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let lista of names) {
+    console.log(lista);
+}
