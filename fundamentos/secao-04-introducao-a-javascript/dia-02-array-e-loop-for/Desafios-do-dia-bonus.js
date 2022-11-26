66c11f7f2f9fcc37fb300de7a6a717d1f5d49d88
Maria Gomes <mari-pgomes@outlook.com>
2022-11-25 21:03:24 -0300

    Lição bonus 3 dos desafios do dia resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/Desafios-do-dia-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/Desafios-do-dia-bonus.js
@@ -15,17 +15,34 @@
 
 //------------------------------------------
 
-//Lição Bonus 1 - Ordena um array em ordem decrescente utilizando o for.
+//Lição Bonus 2 - Ordena um array em ordem decrescente utilizando o for.
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] > numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+// }
+// console.log(numbers);
+
+//------------------------------------------
+
+//Lição Bonus 3 - Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numerosMultiplicados = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (numbers[index] > numbers[secondIndex]) {
-        let position = numbers[index];
-        numbers[index] = numbers[secondIndex];
-        numbers[secondIndex] = position;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (index + 1 < numbers.length) {
+        numerosMultiplicados.push(numbers[index] * numbers[index + 1]);
+    } else {
+        numerosMultiplicados.push(numbers[index] * 2);
       }
-    }
 }
-console.log(numbers);
+console.log(numerosMultiplicados);
