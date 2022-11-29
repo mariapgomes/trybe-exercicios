33645b71a624d68c4e3e72395be735ff6575be20
Maria Gomes <mari-pgomes@outlook.com>
2022-11-29 20:24:49 -0300

    Lição 2 resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/exercicio-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/exercicio-do-dia.js
@@ -11,11 +11,35 @@
 
 // Exercício 2 - Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra.
 
-let word = 'trybe';
-let invertedWord = ""
+// let word = 'trybe';
+// let invertedWord = ""
 
-for (let index = 0; index < word.length; index += 1){
-    invertedWord += word[word.length - 1 - index];
+// for (let index = 0; index < word.length; index += 1){
+//     invertedWord += word[word.length - 1 - index];
     
+// }
+// console.log(invertedWord);
+
+//---------------------------------------------
+
+// Exercício 3 - Imprime a maior palavra e a menor palavra do array.
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
 }
-console.log(invertedWord);
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
