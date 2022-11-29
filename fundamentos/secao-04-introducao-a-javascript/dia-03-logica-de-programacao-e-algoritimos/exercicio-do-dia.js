1d090ff12f382601cde2f77651e0252330e93210
Maria Gomes <mari-pgomes@outlook.com>
2022-11-29 18:18:53 -0300

    Lição 2 resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/exercicio-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/exercicio-do-dia.js
@@ -1,8 +1,21 @@
 // Exercício 1 - Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
 
-let factorial = 1
+// let factorial = 1
 
-for (let index = 10; index > 0; index -= 1){
-    factorial *= index;
+// for (let index = 10; index > 0; index -= 1){
+//     factorial *= index;
+// }
+// console.log(factorial);
+
+//---------------------------------------------
+
+// Exercício 2 - Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra.
+
+let word = 'trybe';
+let invertedWord = ""
+
+for (let index = 0; index < word.length; index += 1){
+    invertedWord += word[word.length - 1 - index];
+    
 }
-console.log(factorial);
+console.log(invertedWord);
