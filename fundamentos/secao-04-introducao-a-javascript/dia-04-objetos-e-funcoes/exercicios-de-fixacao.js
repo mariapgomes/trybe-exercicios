35a93baced5d6a6ae6a4f81964705a3082c84c11
Maria Gomes <mari-pgomes@outlook.com>
2022-11-30 18:34:49 -0300

    Lição 2 resolvida.

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-de-fixacao.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-de-fixacao.js
@@ -5,15 +5,15 @@
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
 
-let player ={
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2, 
-        silver: 3 
-    }
-}
+// let player ={
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2, 
+//         silver: 3 
+//     }
+// }
 
 // console.log(player.name +' ' + player.lastName + ' ' + player.age + ' anos');
 
@@ -21,4 +21,28 @@ let player ={
 
 // console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes ' + player.bestInTheWorld)
 
-console.log('A jogadora possui ' + player. medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
+// console.log('A jogadora possui ' + player. medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
+
+//-------------------------------------
+
+// Lição 2 de fixação
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for (boasVindas in names){
+//     // console.log('Olá ' + names[boasVindas])
+//   }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (key in car){
+    console.log(key, car[key])
+  }
