833e9dcabd7f0c662b1afc8d84d1438e70771c87
Maria Gomes <mari-pgomes@outlook.com>
2022-11-30 16:02:52 -0300

    Cria um objeto dentro de uma variável.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-de-fixacao.js
@@ -0,0 +1,16 @@
+// Lição 1 de fixação
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+let player ={
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals = {
+        golden: 2, 
+        silver: 3 
+    }
+}
