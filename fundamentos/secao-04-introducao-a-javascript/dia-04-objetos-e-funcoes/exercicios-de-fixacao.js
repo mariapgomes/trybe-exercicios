90bab4ee3b75cfb1dbb06898b22e51cdb4d15c80
Maria Gomes <mari-pgomes@outlook.com>
2022-11-30 16:19:28 -0300

    Acessa e imprime as propriedades de uma chave.

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-de-fixacao.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-de-fixacao.js
@@ -17,6 +17,8 @@ let player ={
 
 // console.log(player.name +' ' + player.lastName + ' ' + player.age + ' anos');
 
-player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+// player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
-console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes ' + player.bestInTheWorld)
+// console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes ' + player.bestInTheWorld)
+
+console.log('A jogadora possui ' + player. medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
