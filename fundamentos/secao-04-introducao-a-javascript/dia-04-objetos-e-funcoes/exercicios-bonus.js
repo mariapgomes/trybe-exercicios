11cc30f0b11ee79f41e2137ffb669e05cc1af4c4
Maria Gomes <mari-pgomes@outlook.com>
2022-12-03 12:51:13 -0300

    Desafio 4 parte I concluído.

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicios-bonus.js
@@ -141,6 +141,43 @@
 
 // console.log('Sua Cesta possui:', quantidadeFrutas);
 
+//Desafio 4 - Acesse as chaves nome, sobrenome, andar e apartamento do último morador do blocoDois e faça um console.log no seguinte formato: “O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101”.
+
+
+let moradores = {
+    blocoUm: [
+      {
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+      },
+      {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+      },
+    ],
+    blocoDois: [
+      {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+      },
+      {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+      },
+    ],
+  };
+
+  console.log('O morador do bloco 2 de nome', moradores.blocoDois[1].nome, moradores.blocoDois[1].sobrenome, 'que mora no', moradores.blocoDois[1].andar, 'andar', moradores.blocoDois[1].apartamento + '.' );
+
+
 
 
 
