834c56aa767575a79ef7dcc16172bd36d5ca188c
Maria Gomes <mari-pgomes@outlook.com>
2022-12-04 11:27:13 -0300

    Desafio 10 concluída.

--- fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-e6-let-cont-arrow-function-e-template-literais/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-e6-let-cont-arrow-function-e-template-literais/exercicios-do-dia.js
@@ -170,3 +170,12 @@ let statusMotor = 'Desligado';
 const ligaDesliga = () => (statusMotor === 'Desligado') ? statusMotor = 'Ligado' : statusMotor = 'Desligado';
 
 console.log(`O motor está ${ligaDesliga()}`)
+
+/////////////////////////////////////////////////
+
+// Desafio 10 -  Crie uma função que calcule a área de um círculo.
+
+const pi = 3.14;
+const circleArea = (raio) => pi * raio**2;
+
+console.log(`Essa é a área do círculo: ${circleArea(5)}.`)
